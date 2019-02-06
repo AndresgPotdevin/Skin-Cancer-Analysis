f8de2a531e09b31c1246e403153968c92b43498a
Andres <andresgpotdevin@gmail.com>
2019-02-05 20:43:39 -0500

    base app

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def Home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
