d9968094950a49040b3f4dccfa568809bb275455
Andres <andresgpotdevin@gmail.com>
2019-02-07 21:08:52 -0500

    adding front end

--- app.py
+++ app.py
@@ -56,15 +56,18 @@ def upload_file():
             predictions = predict(filepath)
 
             return jsonify([int(p) for p in predictions])
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+
+    return render_template('home.html')
+
+    # '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <p><input type=file name=file>
+    #      <input type=submit value=Upload>
+    # </form>
+    # '''
 
 
 if __name__ == '__main__':
