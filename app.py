80a7f36dbf75227abdfd16607a3f5dd3ac1ae37f
Andres <andresgpotdevin@gmail.com>
2019-02-07 20:18:47 -0500

    model added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify, render_template
-
+import numpy as np
 import keras
 from keras.preprocessing import image
 from keras import backend as K
@@ -19,17 +19,20 @@ def load_model():
     graph = K.get_session().graph
 
 
-def prepare_image(img):
-    # Convert the image to a numpy array
-    img = image.img_to_array(img)
-    # Scale from 0 to 255
-    img /= 255
-    # Invert the pixels
-    img = 1 - img
-    # Flatten the image to an array of pixels
-    image_array = img.flatten().reshape(-1, 28 * 28)
-    # Return the processed feature array
-    return image_array
+dx = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']
+
+
+def predict(image_path):
+    K.clear_session()
+    model = keras.models.load_model('static/models/sklesion_img.h5')
+    print()
+    image_size = (200, 200)
+    img = image.load_img(image_path, target_size=image_size)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    predictions = list(model.predict(x)[0])
+
+    return predictions
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,40 +44,18 @@ def upload_file():
         print(request)
 
         if request.files.get('file'):
-            # read the file
             file = request.files['file']
 
-            # read the filename
             filename = file.filename
 
-            # create a path to the uploads folder
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-            # Save the file to the uploads folder
             file.save(filepath)
 
-            # Load the saved image using Keras and resize it to the mnist
-            # format of 28x28 pixels
-            image_size = (28, 28)
-            im = image.load_img(filepath, target_size=image_size,
-                                grayscale=True)
+            image_size = (200, 200)
+            predictions = predict(filepath)
 
-            # Convert the 2D image to an array of pixel values
-            image_array = prepare_image(im)
-            print(image_array)
-
-            # Get the tensorflow default graph and use it to make predictions
-            global graph
-            with graph.as_default():
-
-                # Use the model to make a prediction
-                predicted_digit = model.predict_classes(image_array)[0]
-                data["prediction"] = str(predicted_digit)
-
-                # indicate that the request was a success
-                data["success"] = True
-
-            return jsonify(data)
+            return jsonify([int(p) for p in predictions])
     return '''
     <!doctype html>
     <title>Upload new File</title>
